e4c64c32da46c21dd1e0d7a0e21ed706719b0dc8
Alessandro Custodio Marques <alessandrochocolate@hotmail.com>
2023-05-29 19:34:06 -0300

    anatomia das classes 1

---
+++ projeto-java-teste/java_sintaxe/anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+    // Tudo que vai ser escrito na classe
+    public static void main(String[] args) {
+        // método --> tudo será executado aqui
+        
+        System.out.println("\nAula de anatomia das classes 1 - DIO\n");
+        //saída da informação no terminal
+    }
+}
