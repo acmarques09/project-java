5342291c3daddf3b30feabc843513b3e6c07c001
Alessandro Custodio Marques <alessandrochocolate@hotmail.com>
2023-05-30 19:11:01 -0300

    Tipos e Variaveis

--- projeto-java-teste/java_sintaxe/anatomia-classes/src/trila.formativa/dio/anatomia.classes/tipos-variaveis/src/TiposVariaveis.java
+++ projeto-java-teste/java_sintaxe/anatomia-classes/src/trila.formativa/dio/anatomia.classes/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        
-    }
+            byte idade = 45;
+            short ano = 2023;
+            int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+            long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+            float pi = 3.14F; // precisar terminar com F no final para não ser considerada como double
+            double salario = 1275.33;
+            double salarioMinimo = 2500; // declarar somente o numeral sem as casas pós ponto
+            double unidade = 2.65;
+
+            System.out.println(unidade);
+            System.out.println(salario);
+            System.out.println(ano);
+            System.out.println(idade);
+}
 }
