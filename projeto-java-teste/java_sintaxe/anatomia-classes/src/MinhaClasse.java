6de65c58abd7cf36dc48099453b02c7c51e45703
Alessandro Custodio Marques <alessandrochocolate@hotmail.com>
2023-05-30 11:43:07 -0300

    concatenação

--- projeto-java-teste/java_sintaxe/anatomia-classes/src/MinhaClasse.java
+++ projeto-java-teste/java_sintaxe/anatomia-classes/src/MinhaClasse.java
@@ -3,7 +3,17 @@ public class MinhaClasse {
     public static void main(String[] args) {
         // método --> tudo será executado aqui
         
-        System.out.println("\nAula de anatomia das classes 1 - DIO\n");
+        // System.out.println("\nAula de anatomia das classes 1 - DIO\n");
         //saída da informação no terminal
+
+        String primeiroNome = "Alessandro";
+        String segundoNome = "Marques";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+        }
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
